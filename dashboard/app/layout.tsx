bab9a3b3632322f857b4de5b5e3b8c66ae07ad5e
nicolasjott <nicobox2002@gmail.com>
2024-10-09 14:47:24 -0400

    add districts page and change title

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,53 +1,46 @@
-import * as React from 'react';
-import { AppProvider } from '@toolpad/core/nextjs';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import type { Navigation } from '@toolpad/core';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LocalConvenienceStoreIcon from "@mui/icons-material/LocalConvenienceStore";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import type { Navigation } from "@toolpad/core";
+import { AppProvider } from "@toolpad/core/nextjs";
+import * as React from "react";
 
-import theme from '../theme';
+import theme from "../theme";
 
 const NAVIGATION: Navigation = [
   {
-    kind: 'header',
-    title: 'Main items',
+    kind: "header",
+    title: "Main items",
   },
   {
-    segment: '',
-    title: 'Dashboard',
+    segment: "",
+    title: "Dashboard",
     icon: <DashboardIcon />,
   },
   {
-    segment: 'orders',
-    title: 'Orders',
-    icon: <ShoppingCartIcon />,
+    segment: "districts",
+    title: "Districts",
+    icon: <LocalConvenienceStoreIcon />,
   },
 ];
 
 const BRANDING = {
-  title: 'My Toolpad Core Next.js App',
+  title: "Travel Centers of America Dashboard",
 };
 
-
-
 export default function RootLayout(props: { children: React.ReactNode }) {
-  
-
   return (
     <html lang="en" data-toolpad-color-scheme="light" suppressHydrationWarning>
       <body>
-        
-          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-            <AppProvider
-              navigation={NAVIGATION}
-              branding={BRANDING}
-              
-              theme={theme}
-            >
-              {props.children}
-            </AppProvider>
-          </AppRouterCacheProvider>
-        
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <AppProvider
+            navigation={NAVIGATION}
+            branding={BRANDING}
+            theme={theme}
+          >
+            {props.children}
+          </AppProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
