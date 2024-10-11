0685c1745e38ad8508724ad9b7f3fbad62b343ad
Scarletbobcat <tien.hoan314@yahoo.com>
2024-10-11 14:44:26 -0400

    Formatting with new .prettier script

--- dashboard/app/(dashboard)/common-words/page.tsx
+++ dashboard/app/(dashboard)/common-words/page.tsx
@@ -4,48 +4,48 @@ import { Box } from "@mui/material";
 var i = 0;
 
 function createData(word: string, appearances: number) {
-  const data = {
-    id: i,
-    word: word,
-    appearances: appearances,
-  };
-  i += 1;
-  return data;
+    const data = {
+        id: i,
+        word: word,
+        appearances: appearances,
+    };
+    i += 1;
+    return data;
 }
 
 const columns = [
-  { field: "word", headerName: "Word", width: 150 },
-  { field: "appearances", headerName: "Appearances", flex: 1 },
+    { field: "word", headerName: "Word", width: 150 },
+    { field: "appearances", headerName: "Appearances", flex: 1 },
 ];
 
 // later on will want to get data from api with fetch and then convert
 // to array
 const rows = [
-  createData("good", 53435),
-  createData("food", 52033),
-  createData("truck", 42609),
-  createData("place", 41471),
-  createData("great", 40256),
+    createData("good", 53435),
+    createData("food", 52033),
+    createData("truck", 42609),
+    createData("place", 41471),
+    createData("great", 40256),
 ];
 
 export default function CommonWords() {
-  return (
-    <>
-      <Box>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          disableRowSelectionOnClick
-        />
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    disableRowSelectionOnClick
+                />
+            </Box>
+        </>
+    );
 }
