b8216bdf04179cf463086999de67429d96450322
Scarletbobcat <tien.hoan314@yahoo.com>
2024-10-11 14:29:30 -0400

    Changed width to 100%

--- dashboard/app/(dashboard)/common-words/page.tsx
+++ dashboard/app/(dashboard)/common-words/page.tsx
@@ -31,7 +31,7 @@ const rows = [
 export default function CommonWords() {
   return (
     <>
-      <Box sx={{ height: 400, width: 400 }}>
+      <Box sx={{ height: 400, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
