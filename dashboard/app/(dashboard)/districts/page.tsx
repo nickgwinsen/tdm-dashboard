413a5af9cc1b85f58de7a98d845b0b06209265d4
afwingerkailer <wkfordayz@gmail.com>
2024-10-11 14:51:11 -0400

    added district dropdown

--- dashboard/app/(dashboard)/districts/page.tsx
+++ dashboard/app/(dashboard)/districts/page.tsx
@@ -1,5 +1,34 @@
-import Typography from "@mui/material/Typography";
+"use client";
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-export default function DistrictsPage() {
-    return <Typography>Welcome to the Toolpad districts!</Typography>;
+export default function SelectSmall() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Age</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={age}
+        label="District"
+        onChange={handleChange}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>District 1</MenuItem>
+        <MenuItem value={20}>District 2</MenuItem>
+        <MenuItem value={30}>District 3</MenuItem>
+      </Select>
+    </FormControl>
+  );
 }
