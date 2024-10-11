71ce3485ddb011c0cb7fdcb905564f317572ebf3
nicolasjott <nicobox2002@gmail.com>
2024-10-11 14:17:06 -0400

    add prettier and update formatting

--- dashboard/app/(dashboard)/districts/page.tsx
+++ dashboard/app/(dashboard)/districts/page.tsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
 
 export default function DistrictsPage() {
-  return <Typography>Welcome to the Toolpad districts!</Typography>;
+    return <Typography>Welcome to the Toolpad districts!</Typography>;
 }
