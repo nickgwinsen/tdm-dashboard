5cf54d70c95b714ff7214c5f9cd2f102adbf39f3
nicolasjott <nicobox2002@gmail.com>
2024-10-14 14:44:29 -0400

    signup

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 
 from app.etc.crypt import hash_password
-from app.models.user_model import *
+from app.models import UserModel
 from app.schemas.user_schema import UserIn, UserOut
 
 router = APIRouter()
 
 from app.db.session import db
+
 
 @router.post("/login")
 async def auth_login(user: UserIn):
@@ -17,16 +18,20 @@ async def auth_login(user: UserIn):
     print(db.__getstate__())
 
 
+@router.post("/signup")
+async def auth_signup(user: UserIn):
+    if await db.find_one(UserModel, UserModel.email == user.email):
+        raise HTTPException(status_code=400, detail="Email already exists")
 
-# @router.post("/signup")
-# async def auth_signup(user: UserIn, db: SessionDep):
-#     user_hash = hash_password(user.password)
-#     user_to_add = Users(email=user.email, **user_hash)
-#     db.add(user_to_add)
-#     db.commit()
-#     db.refresh(user_to_add)
-#     return user_to_add.email
-#
+    user_to_add = UserModel(
+        email=user.email, hashed_password=hash_password(user.password)
+    )
+
+    db.save(user_to_add)
+
+    return user_to_add.email
+
+
 #
 # @router.patch("/enable-two-factor")
 # async def auth_login(db: Session = Depends(get_session)):
