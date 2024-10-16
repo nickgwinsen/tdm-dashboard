396b040c2255c191d28baa6e76c37d8e880a104d
nicolasjott <nicobox2002@gmail.com>
2024-10-16 14:24:50 -0400

    add more to gitignore

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,50 +1,50 @@
 import AbcIcon from "@mui/icons-material/Abc";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import LocalConvenienceStoreIcon from "@mui/icons-material/LocalConvenienceStore";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import type { Navigation } from "@toolpad/core";
 import { AppProvider } from "@toolpad/core/nextjs";
 import * as React from "react";
 
-import theme from '../theme';
-import { SpeedDialIcon } from '@mui/material';
+import { SpeedDialIcon } from "@mui/material";
+import theme from "../theme";
 
 const NAVIGATION: Navigation = [
-  {
-    kind: 'header',
-    title: 'Main items',
-  },
-  {
-    segment: '',
-    title: 'Dashboard',
-    icon: <DashboardIcon />,
-  },
-  {
-    segment: 'categoryRatings',
-    title: 'Ratings By Category',
-    icon: <SpeedDialIcon />,
-  },
-  {
-      segment: "districts",
-      title: "Districts",
-      icon: <LocalConvenienceStoreIcon />,
-  },
-  {
-      segment: "common-words",
-      title: "Most Common Words",
-      icon: <AbcIcon />,
-  },
-  {
-      segment: "star-ratings",
-      title: "Star Ratings",
-      icon: <StarBorderIcon />,
-  },
+    {
+        kind: "header",
+        title: "Main items",
+    },
+    {
+        segment: "",
+        title: "Dashboard",
+        icon: <DashboardIcon />,
+    },
+    {
+        segment: "categoryRatings",
+        title: "Ratings By Category",
+        icon: <SpeedDialIcon />,
+    },
+    {
+        segment: "districts",
+        title: "Districts",
+        icon: <LocalConvenienceStoreIcon />,
+    },
+    {
+        segment: "common-words",
+        title: "Most Common Words",
+        icon: <AbcIcon />,
+    },
+    {
+        segment: "star-ratings",
+        title: "Star Ratings",
+        icon: <StarBorderIcon />,
+    },
     {
         segment: "Business-Actions",
         title: "Business Actions",
-        icon: <BusinessCenterIcon/>,
+        icon: <BusinessCenterIcon />,
     },
 ];
 
